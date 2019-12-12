eb2f063f16d43e849eca6c9858dabf461529f930
DiePalmaF <dnpalma@alumnos.ubiobio.cl>
2019-12-12 12:46:00 -0300

    Update sidebar.blade.php

--- resources/views/plantilla/sidebar.blade.php
+++ resources/views/plantilla/sidebar.blade.php
@@ -5,42 +5,42 @@
                         NAVEGACIÓN
                     </li>
                     <li @click="menu=0" class="nav-item">
-                        <a class="nav-link active" href="main.html"><i class="icon-speedometer"></i>Inicio</a>
+                        <a class="nav-link active" href="main.html"><i class="icon-home"></i>Inicio</a>
                     </li>
                     
                     <li class="nav-item nav-dropdown">
-                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-bag"></i>Control de Clientes</a>
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-people"></i>Control de Clientes</a>
                         <ul class="nav-dropdown-items">
                             <li @click="menu=1" class="nav-item">
-                                <a class="nav-link" href="#"><i class="icon-bag"></i>Listado de Clientes</a>
+                                <a class="nav-link" href="#"><i class="icon-user"></i>Listado de Clientes</a>
                             </li>
                             <li @click="menu=2" class="nav-item">
-                                <a class="nav-link" href="#"><i class="icon-bag"></i>Listado de Socios</a>
+                                <a class="nav-link" href="#"><i class="icon-user-following"></i>Listado de Socios</a>
                             </li>
                         </ul>
                     </li>
                     <li class="nav-item nav-dropdown">
-                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-wallet"></i>Control de Transacciones</a>
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-basket-loaded"></i>Control de Transacciones</a>
                         <ul class="nav-dropdown-items">
                             <li @click="menu=3" class="nav-item">
-                                <a class="nav-link" href="i#"><i class="icon-wallet"></i>Realizar Transacciones</a>
+                                <a class="nav-link" href="i#"><i class="icon-note"></i>Realizar Transacciones</a>
                             </li>
                             <li @click="menu=4" class="nav-item">
-                                <a class="nav-link" href="#"><i class="icon-notebook"></i>Últimas Transacciones</a>
+                                <a class="nav-link" href="#"><i class="icon-calculator"></i>Últimas Transacciones</a>
                             </li>
                         </ul>
                     </li>
                     <li class="nav-item nav-dropdown">
-                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-basket"></i>Control de Inventario</a>
+                        <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-list"></i>Control de Inventario</a>
                         <ul class="nav-dropdown-items">
                             <li @click="menu=5" class="nav-item">
-                                <a class="nav-link" href="i#"><i class="icon-basket-loaded"></i>Productos</a>
+                                <a class="nav-link" href="i#"><i class="icon-ghost"></i>Productos</a>
                             </li>
                             <li @click="menu=6" class="nav-item">
-                                <a class="nav-link" href="#"><i class="icon-notebook"></i>Géneros</a>
+                                <a class="nav-link" href="#"><i class="icon-puzzle"></i>Géneros</a>
                             </li>
                             <li @click="menu=7" class="nav-item">
-                                <a class="nav-link" href="#"><i class="icon-notebook"></i>Plataformas</a>
+                                <a class="nav-link" href="#"><i class="icon-game-controller"></i>Plataformas</a>
                             </li>
                         </ul>
                     </li>
