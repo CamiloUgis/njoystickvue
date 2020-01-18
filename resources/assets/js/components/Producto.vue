77d20b1251d8b8e1fb69a08c68dd577ca6d2d56c
DiePalmaF <dnpalma@alumnos.ubiobio.cl>
2020-01-17 23:23:13 -0300

    intento select2

--- resources/assets/js/components/Producto.vue
+++ resources/assets/js/components/Producto.vue
@@ -118,7 +118,24 @@
                                         </select>
                                     </div>
                                 </div>
+                                
+                                <div id="el"></div>
 
+                                <!-- using string template here to work around HTML <option> placement restriction -->
+                                <script type="text/x-template" id="demo-template">
+                                <div>
+                                    <p>Selected: {{ selected }}</p>
+                                    <select2 :options="options" v-model="selected">
+                                    <option disabled value="0">Select one</option>
+                                    </select2>
+                                </div>
+                                </script>
+
+                                <script type="text/x-template" id="select2-template">
+                                <select>
+                                    <slot></slot>
+                                </select>
+                                </script>
 
 
                                 <div class="form-group row">
@@ -169,7 +186,9 @@
 </template>
 
 <script>
+
     export default {
+        
         data(){
             return{
                 idProductos:0,
@@ -199,9 +218,19 @@
                 criterio: 'nombre',
                 buscar : '',
                 arrayPlataformas:[],
-                arrayGeneros:[]
+                arrayGeneros:[],
+                selected: null,
+                generitos: [],
+                selected: 2,
+                options: [
+                { id: 1, text: 'Hello' },
+                { id: 2, text: 'World' }
+                ]
 
             }
+        },
+        ready() {
+            this.selectGeneritos()
         },
         computed:{
             isActived: function(){
@@ -378,13 +407,59 @@
                 }
                 this.selectGeneros();
                 this.selectPlataformas();
+            },
+            selectGeneritos: function () {
+            this.$http({
+                url: '/generos/selectGeneros',
+                method: 'GET'
+            }).then(function (response) {
+                this.$set('generitos', response.data)
+            }, function (response) {
+                // error callback
+            });
             }
         },
-        
-        mounted() {
-            this.listarProducto(1, this.buscar, this.criterio);
-        }
+        directives: {
+        select2: {
+            props: ['options', 'value'],
+            template: '#select2-template',
+            mounted: function () {
+                var vm = this
+                $(this.$el)
+                // init select2
+                .select2({ data: this.options })
+                .val(this.value)
+                .trigger('change')
+                // emit event on change.
+                .on('change', function () {
+                    vm.$emit('input', this.value)
+                })
+  },
+  watch: {
+    value: function (value) {
+      // update value
+      $(this.$el)
+      	.val(value)
+      	.trigger('change')
+    },
+    options: function (options) {
+      // update options
+      $(this.$el).empty().select2({ data: options })
     }
+  },
+  destroyed: function () {
+    $(this.$el).off().select2('destroy')
+  }
+}
+},
+
+ 
+  
+  
+    
+ 
+  
+
 </script>
 <style>
     .modal-content{
